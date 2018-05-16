d35e99593fdac719bda993bdfa6935dfad38fc3f
Liliane <liliane@happybiz.com.br>
2018-05-16 17:49:02 -0300

    Ajuste da função buscar do blog, e da pag de post

---
+++ public/js/blog.js
@@ -0,0 +1,16 @@
+"use strict";
+(function($){
+
+    $(".lupa").on("click", function(){
+        $("#busca").fadeToggle(50);
+    });
+
+    $(".botao-procurar").on("click", function(){
+        $("#busca").fadeToggle(50);
+    });
+
+    $(".fechar-busca").on("click", function(){
+        $("#busca").fadeToggle(50);
+    });
+
+})($)
