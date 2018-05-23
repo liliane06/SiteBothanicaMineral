5c69fc14728ebcb40fc48ae5bb34135e9c3d2b4c
Liliane <liliane@happybiz.com.br>
2018-05-23 15:53:49 -0300

    ajuste das pag de categorias

---
+++ public/js/produtos.js
@@ -0,0 +1,12 @@
+"use strict";
+(function($){
+
+    $("#chk0008").on('click', function(){
+        $(".linha-profissional").toggle(1000);
+    });
+
+    $("#chk0007").on('click', function(){
+        $(".linha-cliente").toggle(1000);
+    });
+
+})($)
